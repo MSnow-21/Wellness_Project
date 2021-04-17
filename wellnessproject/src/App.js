a9bafa3211cabf1c6c4cf9ce6919b06c0477cb49
Ryan Wilson <RryanWilsonW@gmail.com>
2021-04-17 17:44:55 -0400

    Basic router set up with mock link buttons.

--- wellnessproject/src/App.js
+++ wellnessproject/src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
-
+// We are getting our router tools from react-router-dom
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Nav from './components/Nav';
+import Test1 from './components/pages/Test1';
+import Test2 from './components/pages/Test2';
 function App() {
   return (
     <div className="App">
-      
+      {/* We are setting up our router, and defining different routes to render our page depending on the websites url.*/}
+      <Router>
+        <Nav />
+        <Switch>
+          {/* To add more routes, simply follow the syntax within the switch statement.*/}
+            <Route exact path={['/', '/test1']}>
+              <Test1 />
+            </Route>
+            <Route exact path='/test2'>
+              <Test2 />
+            </Route>
+          </Switch>
+      </Router>
     </div>
   );
 }
---
+++ wellnessproject/src/components/pages/Test1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Test1 = () => {
+    return (
+        <div>
+            <p>This is the first page.</p>
+        </div>
+    )
+}
+
+export default Test1;
