c4ee00e79fade5682aa0bc4011933592aad5d517
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-04-30 14:45:42 -0400

    Created useEffect in IndexPageOne function. Imported in API from utils. Created utils folder in src folder. Added API.js to utils folder.

--- wellnessproject/client/src/components/SearchOneIndex/index.js
+++ wellnessproject/client/src/components/SearchOneIndex/index.js
@@ -1,8 +1,26 @@
 import React, { useEffect, useState} from "react";
 import SearchFormOne from "../SearchFormOne/index";
+import API from "../../utils/API";
 
 const IndexPageOne = () => {
     const [search, setSearch] = useState("");
     const [item, setItems] = useState([]);
     const [error, setError] = useState("");
+    
+    useEffect(() => {
+        if(!search){
+            return;
+        }
+
+        API.getData(search)
+        .then((res)=>{
+            console.log(res.data)
+            setItems(res.data)
+        })
+        .catch(err=> setError(err));
+    },[search]);
+
+
+
+    
 }
