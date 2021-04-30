2435357da58483a2c109605c9bc63ffc5bce756b
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-04-30 14:36:43 -0400

    Created SearchOneIndex component. Added index.js to component folder. Imported React requiremets. Set up initial IndexPageOne function

---
+++ wellnessproject/client/src/components/SearchOneIndex/index.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState} from "react";
+import SearchFormOne from "../SearchFormOne/index";
+
+const IndexPageOne = () => {
+    const [search, setSearch] = useState("");
+    const [item, setItems] = useState([]);
+    const [error, setError] = useState("");
+}
