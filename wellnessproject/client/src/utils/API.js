1e1515e35bce9435b2e90c5ebee6c262742ba878
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-05-01 07:50:34 -0400

    Created constant in API settings and added to API call.

--- wellnessproject/client/src/utils/API.js
+++ wellnessproject/client/src/utils/API.js
@@ -1,10 +1,16 @@
 import axios from "axios";
 
 
+//USDA SEARCH QUERY REQUIREMENTS
+
 const USDA_BASEURL = `${process.env.REACT_APP_USDA_BASE_URL}`;
 const API_ASK = "?api_key=";
 const USDA_API_KEY = `${process.env.REACT_APP_USDA_API_KEY}`;
 const query = "&query=";
+
+
+//Rapid API Query REQUIREMENTS
+const RAPID_API_KEY = `${process.env.REACT_APP_RAPID_API_KEY}`
 
 export default{
     getData: (search) => 
@@ -18,7 +24,7 @@ export default{
             'method':'GET',
             'url':'https://edamam-food-and-grocery-database.p.rapidapi.com/parser',
             'headers': {
-                'x-rapidapi-key':'',
+                'x-rapidapi-key':RAPID_API_KEY,
                 'x-rapidapi-host':'edamam-food-and-grocery-database.p.rapidapi.com'
             },
             'params': {'ingr': search},
@@ -29,7 +35,7 @@ export default{
             'method':'GET',
             'url': 'https://nutritionix-api.p.rapidapi.com/v1_1/search/'+search,
             'headers': {
-            'x-rapidapi-key':'',
+            'x-rapidapi-key':RAPID_API_KEY,
             'x-rapidapi-host':'nutritionix-api.p.rapidapi.com'
             },
             'params': {'fields': 'item_name,item_id,brand_name,nf_calories,nf_total_fat'},
