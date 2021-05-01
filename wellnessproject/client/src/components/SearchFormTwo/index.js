8bb06deb1ad400b024029e3651166363992e50e4
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-04-30 21:53:49 -0400

    Created components for SearchFormTwo, SearchResultsTwo, SearchTwoIndex. Added jsx to SearchFormTwo

---
+++ wellnessproject/client/src/components/SearchFormTwo/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function SearchFormTwo(props){
+    return(
+      <form className="search">
+        <div className="form-group">
+            <label htmlFor="language">Search:</label>
+              <input
+              value={props.search}
+              onChange={props.handleInputChangeTwo}
+              name="term"
+              type="term"
+              className="form-control"
+              placeholder="Search for Data"
+              id="search"
+              />
+            </div>
+        </form>
+    );
+}
+
+export default SearchFormTwo;
