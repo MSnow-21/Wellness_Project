f77f823e1f512b84052ccaf3b76c765a070c5691
Chris Curry <Chamilitary216@hotmail.com>
2021-05-11 01:57:25 -0400

    added button

--- client/src/pages/Homepage/index.js
+++ client/src/pages/Homepage/index.js
@@ -11,7 +11,7 @@ function Header () {
                 <h2>LETS GET YOUR</h2>
                 <h1><span>MIND AND BODY</span> TOGETHER</h1>
                 <p className = "details">"Don't make the body do what the spirit does best, and don't put a big load on the spirit that the body could easily carry."</p>
-                {/* <div href='#' className = 'header-btn'>DAILY LOG</div> */}
+                <div href='#' className = 'header-btn'>DAILY LOG</div>
             </div>
 
 
